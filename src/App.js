55619ef472f23874638e679ae29248fc9dad74db
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-24 20:47:01 +0530

    add post form created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import AddPost from './components/AddPost';
 
 function App() {
   return (
     <div>
       <Signup/>
       <Login/>
+      <AddPost/>
     </div>
   );
 }
---
+++ src/components/AddPost.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const AddPost = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <center>
+            <h3>Add Post</h3>
+          </center>
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">Title</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">Content</label>
+                <textarea name="" id="" className="form-control"></textarea>
+              </div>
+              <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <center>
+                <button className="btn btn-info">Post</button>
+                </center>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddPost
